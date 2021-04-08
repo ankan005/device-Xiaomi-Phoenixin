ca4b1e20483f5e1144a0d0a9bb93f33b9f16b93c
Rasenkai <rasenkai99@gmail.com>
2021-04-08 10:58:07 +0000

    phoenix: Add audio_policy_configuration from common
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_overlay_dynamic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_overlay_dynamic.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_overlay_static.xml \
