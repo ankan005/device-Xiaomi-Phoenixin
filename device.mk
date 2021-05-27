3619791179dbcdead700474733f0a97b6f8323c6
ankan005 <ankan005@gmail.com>
2021-05-27 04:11:11 +0000

    correct power packages names

--- device.mk
+++ device.mk
@@ -414,7 +414,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service \
+    android.hardware.power-impl
 
 # Protobuf
 PRODUCT_PACKAGES += \
